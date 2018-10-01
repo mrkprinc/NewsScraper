26fd6b4b38ce5d0b8e5e633f485c8fb03b6edd99
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-09-30 21:08:15 -0400

    savepoint

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const fs = require('file-system');
+
 const db = require("./models");
 
 const PORT = 3000;
@@ -8,7 +10,7 @@ const app = express();
 const router = express.Router();
 require('./controller/controller.js')(router);
 
-const hbs = exphbs.create({defaultLayout: 'main', partialsDir: 'views/_partials/'})
+const hbs = exphbs.create({defaultLayout: 'main', extname: '.hbs', partialsDir: 'views/_partials/'})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', hbs.engine);
@@ -16,10 +18,8 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 hbs.getPartials({precompiled: true}).then(function(partials) {
-  // TEMP
-  console.log(partials);
+  fs.writeFileSync('public/js/hbs-templates.js', JSON.stringify(partials, null, 2));
+  app.listen(PORT, function() {
+    console.log("Ready at localhost:" + PORT);
+  });
 })
-
-app.listen(PORT, function() {
-  console.log("Ready at localhost:" + PORT);
-});
