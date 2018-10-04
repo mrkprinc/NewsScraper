44029ad38f41f8984eccd340e2963ab2845b710e
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-03 21:10:42 -0400

    savepoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 
